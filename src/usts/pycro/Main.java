7985e82ed6c372b1f63694d203abff27ed178fb6
Pycro <pycro6815@163.com>
2023-07-30 21:01:44 +0800

    7.30 update

--- src/usts/pycro/Main.java
+++ src/usts/pycro/Main.java
@@ -7,50 +7,16 @@ package usts.pycro;
  */
 public class Main {
     public static void main(String[] args) {
-        A a = new B();
-        
+        System.out.println(digitSum(1));
+    }
+
+    public static int digitSum(int num) {
+        int res = 0;
+        while (num > 0) {
+            res += num % 10;
+            num /= 10;
+        }
+        return res;
     }
 }
 
-class A {
-    static {
-        System.out.println("static code block of A");
-    }
-
-    protected String name;
-
-    public A(String str) {
-        System.out.println("A init with arg: " + str);
-    }
-
-    protected void eat() {
-        System.out.println("A eat");
-    }
-}
-
-class B extends A {
-    static {
-        System.out.println("static code block of B");
-    }
-
-    protected String name;
-
-    protected B() {
-        this(null);
-    }
-
-    protected B(String str) {
-        super("oneArgConstructor");
-        System.out.println("B init with arg: " + str);
-    }
-
-    // @Override
-    protected void eat() {
-        System.out.println("B eat");
-    }
-
-    private void drink() {
-        super.eat();
-        System.out.println("B drink");
-    }
-}
